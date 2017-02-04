397c2cfef3ff336a2228e5d1939f8e161d8fb98f
Juraj Repcik <yourigh.juraj@gmail.com>
2017-02-04 18:56:14 +0100

    Add files via upload

---
+++ _170127_AnalogChannels_to_UART/_170127_AnalogChannels_to_UART.ino
@@ -0,0 +1,35 @@
+
+int ledPin = 13;      // select the pin for the LED
+
+int val = 0;  // variable to store the value coming from the sensor
+bool ledstate = false;
+int ADCs[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+char head[8][4] = {"A0:", "A1:", 
+                  "A2:", "A3:", 
+                  "A4:", "A5:", 
+                  "A6:", "A7:"};
+int i=0;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin (9600);
+}
+
+void loop() {
+
+  val = analogRead(ADCs[i]);
+  Serial.print (head[i]);
+  Serial.print (val);  
+  Serial.print ("*");
+  if (i != 7) {
+    i++;
+  }else{
+    i = 0;
+  }
+    
+  // toggle LED
+  digitalWrite(ledPin, ledstate);
+  ledstate = not ledstate;
+  //delay(500);
+}
